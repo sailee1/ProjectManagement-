a9aa845ccc248c409e395ad6854dee7f52a03c03
sailee1 <saiclee1986@googlemail.com>
2022-06-14 07:21:02 +0100

    Started adding frontend functionallity

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const colors = require('colors')
+const cors = require('cors')
 require('dotenv').config()
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
@@ -9,9 +10,12 @@ const port = process.env.PORT || 5000;
 
 const app = express()
 
+
 //Connect to database 
 connectDB()
 
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
 schema, 
